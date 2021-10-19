f2a685c57fbc34cce3039d7e60ae9acf8067d291
DHEERAJ <dheeraj.pasupuleti@itversity.com>
2021-10-19 16:45:03 +0530

    Updated code for processing data

--- processing_file.py
+++ processing_file.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
 
-file_path = 'C:/Users/DHEERAJ/PycharmProjects/hs-energy-dl/data/heb_oil.csv'
-
-
-def pandas_processing():
+def pandas_processing(file_path):
     read_file = pd.read_csv(file_path, header=0, delimiter=',')
     read_file_rename = read_file.rename(
         columns={'Oil (m3)': 'crude_oil (m3)', 'Gas (103m3)': 'natural_gas (km3)', 'Water (m3)': 'other (m3)',
@@ -18,4 +15,4 @@ def pandas_processing():
     return print(structured_df)
 
 
-pandas_processing()
+pandas_processing(r'C:\Users\DHEERAJ\PycharmProjects\hs-energy-dl\data\heb_oil.csv')
