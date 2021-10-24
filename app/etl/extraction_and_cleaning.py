714067074f4b692e86d18866a7819877bff471a7
DHEERAJ <dheeraj.pasupuleti@itversity.com>
2021-10-25 00:10:24 +0530

    Code for extracting the data and cleaning the data.

--- app/etl/extraction_and_cleaning.py
+++ app/etl/extraction_and_cleaning.py
@@ -1,14 +1,17 @@
 import os
+from dotenv import load_dotenv
 import camelot
 import pandas as pd
 import numpy as np
 from app.utils.logging_init import init_logger
+load_dotenv()
 
 logger = init_logger()
+folder_path = os.getenv('BASE_DATA_PATH')
+
 
 def pre_cleaning(new_tables):
     if len(new_tables.columns) == 7:
-        # print(new_tables[~new_tables['Total'].str.contains("Yearly")])
         new_tables.set_index('Total').filter(like='Yearly', axis=0)
         new_tables = new_tables.drop('Total', axis=1)
     else:
@@ -24,14 +27,12 @@ def final_cleaning(sample_df):
     return dropped_null
 
 
-def extraction(path):
-    for path, dirs, files in os.walk(path):
+def extraction(folder_path):
+    for path, dirs, files in os.walk(folder_path):
         for file in files:
-            # logger.info(file)
             filename = os.path.join(path, file)
             tables = camelot.read_pdf(filename, pages='all', flavor='stream', edge_tol=1000)
             table_number = tables.n
-            # logger.info(table_number)
             all_tables = pd.DataFrame()
             for table in range(table_number):
                 temp_df = tables[table].df
@@ -39,21 +40,24 @@ def extraction(path):
                 if column_len == 6:
                     temp_df.columns = ['Well Name', 'Year', 'Month', 'Oil_(m³)', 'Gas_(10³m³)', 'Water_(m³)']
                     all_tables = pd.concat([all_tables, temp_df])
+                    all_tables = all_tables[~all_tables['Well Name'].str.contains('Well Name')]
                 elif column_len == 5:
                     temp_df['Well Name'] = np.NAN
                     temp_df.columns = ['Well Name', 'Year', 'Month', 'Oil_(m³)', 'Gas_(10³m³)', 'Water_(m³)']
                     all_tables = pd.concat([all_tables, temp_df])
+                    all_tables = all_tables[~all_tables['Well Name'].str.contains('Well Name')]
                 else:
                     temp_df.columns = ['Well Name', 'Year', 'Month', 'Total', 'Oil_(m³)', 'Gas_(10³m³)', 'Water_(m³)']
-                all_tables = pd.concat([all_tables, temp_df])
-                all_tables = all_tables[~all_tables['Well Name'].str.contains('Well Name')]
-                pre_cleaning_data = pre_cleaning(all_tables)
-
-                final_cleaning_data = final_cleaning(pre_cleaning_data)
-                print(final_cleaning_data)
-                # final_cleaning_data.to_csv(r'C:\Users\DHEERAJ\PycharmProjects\hs-energy-dl\data\csv_file.csv', header=True)
+                    all_tables = pd.concat([all_tables, temp_df])
+                    all_tables = all_tables[~all_tables['Well Name'].str.contains('Well Name')]
+            pre_cleaning_data = pre_cleaning(all_tables)
+            final_cleaning_data = final_cleaning(pre_cleaning_data)
+            final_cleaning_data['Month'] = '01'+ '-'+ final_cleaning_data['Month'] + '-' + final_cleaning_data['Year']
+            final_cleaning_data = final_cleaning_data.drop(columns='Year', axis=1)
+            print(final_cleaning_data)
+            final_cleaning_data.to_csv(r'C:\Users\DHEERAJ\PycharmProjects\hs-energy-dl\data\csv_file.csv', header=True)
 
     return final_cleaning_data
 
 
-extraction(r'C:\Users\DHEERAJ\PycharmProjects\hs-energy-dl\Hibernia')
+extraction(folder_path)
