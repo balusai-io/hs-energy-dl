8a0645a40319c7d1ea5cd8e8fceab93f5562312e
DHEERAJ <dheeraj.pasupuleti@itversity.com>
2021-10-28 00:01:09 +0530

    Code for processing the extracted data

--- app/etl/processing_file.py
+++ app/etl/processing_file.py
@@ -6,6 +6,7 @@ logger = init_logger()
 units_table = pd.read_csv(r'C:\Users\DHEERAJ\PycharmProjects\hs-energy-dl\tbl_unit.csv', header=0, sep=',')
 product_table = pd.read_csv(r'C:\Users\DHEERAJ\PycharmProjects\hs-energy-dl\energy_product.csv', header=0, sep=',')
 
+
 def file_processing(file_path, units_table, product_table):
     try:
         read_file = pd.read_csv(file_path)
@@ -15,17 +16,14 @@ def file_processing(file_path, units_table, product_table):
         # adding columns- 'energy' and 'units' to dataframe.
         structured_df[['energy', 'units']] = structured_df['Commodity'].str.split('(', expand=True)
         structured_df['units'] = structured_df['units'].str.replace('[)]', '', regex=True)
-        #calling Id's corresponding to attributes in unit and product id columns
+        # calling Id's corresponding to attributes in unit and product id columns
         structured_df['energy_product_id'] = structured_df.energy.str.lower().map(product_table.set_index('energy_product')['ID'])
         structured_df['energy_unit_id'] = structured_df.units.str.lower().map(units_table.set_index('uom')['unit_of_measure_id'])
-        structured_df = structured_df.drop('energy', axis=1)
-        structured_df = structured_df.drop('units', axis=1)
-        structured_df = structured_df.drop('Commodity', axis=1)
+        structured_df = structured_df.drop(['energy', 'units', 'Commodity'], axis=1)
         print(structured_df)
         return structured_df
     except Exception as e:
         logger.error(e)
-
 
 
 file_processing(r'C:\Users\DHEERAJ\PycharmProjects\hs-energy-dl\data\csv_file.csv', units_table, product_table)
