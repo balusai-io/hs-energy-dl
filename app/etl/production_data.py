d0f7ac41718ac62faeee1116bfbab7cea492b2c9
DHEERAJ <dheeraj.pasupuleti@itversity.com>
2021-11-12 17:45:00 +0530

    Code for inserting production table data

---
+++ app/etl/production_data.py
@@ -0,0 +1,29 @@
+from app.utils import dbconnection
+import pandas as pd
+from app.utils.logging_init import init_logger
+from app.etl.processing_file import file_processing
+import sqlalchemy
+
+logger = init_logger()
+
+def production_update_table(structured_df):
+    #updating production lookup table
+    try:
+        conn = dbconnection.db_connection()
+        structured_df.to_sql(
+            'tbl_cnlopb_production',
+            con=conn,
+            if_exists='append',
+            method='multi',
+            index=False,
+            schema='collections',
+            dtype={"well_id": sqlalchemy.types.INT,
+                   "energy_product_id": sqlalchemy.types.INT,
+                   "unit_of_measure_id": sqlalchemy.types.INT,
+                   "month": sqlalchemy.types.DATE,
+                   "value": sqlalchemy.types.INT,
+                   "field_id": sqlalchemy.types.CHAR(length=5)
+                   }
+        )
+    except Exception as e:
+        logger.error(e)
